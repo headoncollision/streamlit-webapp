35dfb57e98b2ab7f1932805c90d766004bc54537
headoncollision <98327837+headoncollision@users.noreply.github.com>
2023-04-19 23:23:08 +0530

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title('Webapp to calculate largest of 3 numbers')
+
+st.header('_Enter your inputs here:_ ')
+
+number1 = st.number_input('Insert first number')
+number2 = st.number_input('Insert second number')
+number2 = st.number_input('Insert third number')
+
+lis = [number1 , number2 , number3]
+
+a = max(lis)
+
+st.write('The largest number is ' , a)
