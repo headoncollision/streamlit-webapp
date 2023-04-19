7c33cb9cf4c875dbefd41c809d4c127b8a1e1a83
headoncollision <98327837+headoncollision@users.noreply.github.com>
2023-04-19 23:41:17 +0530

    kill me pls

--- streamlit.py
+++ streamlit.py
@@ -8,11 +8,11 @@ number1 = st.number_input('Insert first number')
 number2 = st.number_input('Insert second number')
 number2 = st.number_input('Insert third number')
 
-def maximum(n1,n2,n3):
+def maximum():
   lis = [number1 , number2 , number3]
   a = max(lis)
   st.success(f"Maximum = {a}")
 
 if st.button("Calculate result"):
-    maximum(number1 , number2 , number3)
+    maximum()
 
